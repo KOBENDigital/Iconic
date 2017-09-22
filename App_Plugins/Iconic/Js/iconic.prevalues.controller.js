4d308caf83d0793735606e6e7d75dc7225a10b5b
Mario Lopez <mario@koben.com.au>
2017-09-23 06:35:58 +1000

    added preconfig

--- App_Plugins/Iconic/Js/iconic.prevalues.controller.js
+++ App_Plugins/Iconic/Js/iconic.prevalues.controller.js
@@ -12,6 +12,8 @@
     $scope.currentItem;
     $scope.analysing = "init";
     $scope.newItemFormErrors = [];
+    $scope.configType = "custom";
+    $scope.selectedPreConfig = {};
 
     $scope.addNewItem = function () {
 
@@ -24,7 +26,10 @@
         if (!$scope.newItem.selector) $scope.newItemFormErrors.selector = true;
         if (!$scope.newItem.cssfile) $scope.newItemFormErrors.cssfile = true;
 
-        if (Object.keys($scope.newItemFormErrors).length > 0) return;
+        if (Object.keys($scope.newItemFormErrors).length > 0) {
+            $scope.analysing = "error";
+            return;
+        }
 
         extractStyles($scope.newItem, function () {
             $scope.newItem.alias = uuid();
@@ -35,89 +40,89 @@
             $scope.newItemFormErrors.selector = true;
             $scope.newItemFormErrors.cssfile = true;
             $scope.analysing = "error";
-        });                                  
+        });
 
     }
 
 
-$scope.selectItem = function (item) {
-    if ($scope.currentItem === item)
-        $scope.currentItem = null;
-    else
-        $scope.currentItem = item;
-}
-
-$scope.removeItem = function (index) {
-    $scope.model.value.splice(index, 1);
-}
-
-$scope.hideItemForm = function () {
-    $scope.showNewItemForm = false;
-}
-
-
-function extractStyles(item, successCallback, errorCallback) {
-
-    if (!item.selector || item.selector.length <= 0) {
-        errorCallback();
+    $scope.selectItem = function (item) {
+        if ($scope.currentItem === item)
+            $scope.currentItem = null;
+        else
+            $scope.currentItem = item;
     }
 
-    $http.get(item.cssfile).success(function (data) {
-        item.extractedStyles = [];
-        var pattern = new RegExp(item.selector, 'g');
+    $scope.removeItem = function (index) {
+        $scope.model.value.splice(index, 1);
+    }
 
-        var match = pattern.exec(data);
-        while (match !== null) {
-            item.extractedStyles.push(match[1])
-            match = pattern.exec(data);
-        }
 
-        if (item.extractedStyles.length > 0) {
-            successCallback();
-        } else {
+
+    $scope.selectPreConfig = function (config) {
+        Object.assign($scope.newItem, config);
+    }
+
+    $scope.hideItemForm = function () {
+        $scope.showNewItemForm = false;
+    }
+
+
+    function extractStyles(item, successCallback, errorCallback) {
+
+        if (!item.selector || item.selector.length <= 0) {
             errorCallback();
-
         }
 
-    }).error(function (response) {
-        errorCallback();
-    })
+        $http.get(item.cssfile).success(function (data) {
+            item.extractedStyles = [];
+            var pattern = new RegExp(item.selector, 'g');
 
-}
+            var match = pattern.exec(data);
+            while (match !== null) {
+                item.extractedStyles.push(match[1])
+                match = pattern.exec(data);
+            }
 
-function uuid() {
-    var uuid = "", i, random;
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
+            if (item.extractedStyles.length > 0) {
+                successCallback();
+            } else {
+                errorCallback();
 
-        if (i == 8 || i == 12 || i == 16 || i == 20) {
-            uuid += "-"
-        }
-        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+            }
+
+        }).error(function (response) {
+            errorCallback();
+        })
+
     }
-    return uuid;
-}
 
-    //$scope.model.value = [{
-    //    alias: 'glyphicons',
-    //    name: 'Glyphicons',
-    //    selector: '\\.(glyphicon-[\\w-]+):before{',
-    //    extraClasses: 'glyphicon',
-    //    cssfile: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
-    //}, {
-    //    alias: 'fa',
-    //    name: 'Font Awesome',
-    //    selector: '\\.(fa-[\\w-]+):before{',
-    //    extraClasses: 'fa',
-    //    cssfile: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
-    //},
-    //{
-    //    alias: 'fi',
-    //    name: 'Foundation Icons',
-    //    selector: '\\.(fi-[\\w-]+):before{',
-    //    extraClasses: '',
-    //    cssfile: 'https://cdn.jsdelivr.net/foundation-icons/3.0/foundation-icons.min.css'
-    //}];
+    function uuid() {
+        var uuid = "", i, random;
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+                uuid += "-"
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    }
+
+    $scope.preconfig = [{
+        name: 'Glyphicons',
+        selector: '\\.(glyphicon-[\\w-]+):before{',
+        extraClasses: 'glyphicon',
+    }, {
+        name: 'Font Awesome',
+        selector: '\\.(fa-[\\w-]+):before{',
+        extraClasses: 'fa',
+    },
+    {
+        name: 'Foundation Icons',
+        selector: '\\.(fi-[\\w-]+):before{',
+        extraClasses: '',
+    }];
 
 
 }]);
