c5132b6709a7a1f4a340e83571bfe24ac114749b
Mario Lopez <mario@koben.com.au>
2017-09-29 09:31:16 +1000

    restore broken repo files

---
+++ ValueConverters/IconicValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Koben.Iconic.ValueConverters
+{
+    [PropertyValueType(typeof(HtmlString))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class IconicValueConverter : PropertyValueConverterBase
+    {
+
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("koben.iconic");
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source == null) return string.Empty;
+            var obj = JObject.Parse((string)source);
+            return new HtmlString(obj["iconDisplay"].ToString());
+        }
+    }
+}
