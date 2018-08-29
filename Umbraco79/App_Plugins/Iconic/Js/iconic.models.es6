721478cec4c3a7f8fe3c3054c1cf20be32f43532
Mario Lopez <mario@koben.com.au>
2018-08-29 15:12:55 +1000

    include entire solution in repo

---
+++ Umbraco79/App_Plugins/Iconic/Js/iconic.models.es6
@@ -0,0 +1,33 @@
+﻿class Icon {
+    constructor(icon, packageId) {
+        this.icon = icon || "";
+        this.packageId = packageId || "";
+    }
+}
+
+class Package {
+    constructor() {
+        this.id = this.uuid();
+        this.name = "";
+        this.selector = "";
+        this.template = '<i class="{icon}"></i>';  
+        this.overrideTemplate = false;
+        this.backofficeTemplate = "";
+        this.cssfile = "";
+        this.sourcefile = "";
+        this.extractedStyles = [];
+    }
+
+    uuid() {
+        var uuid = "", i, random;
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+                uuid += "-"
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    }
+}
---
+++ Umbraco79/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Umbraco79")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Umbraco79")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("81c539a8-029f-40a0-bb8a-ce25d8eb519a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
