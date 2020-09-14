9a19004175d0fd1beb2c7830a8dc37f5e8577acb
Mario Lopez <mario@monkii.com>
2020-09-14 21:39:55 +1000

    updated package details

--- Tools/build.ps1
+++ Tools/build.ps1
@@ -2,27 +2,27 @@
 
 # You have to intall CSSetup first: https://devblogs.microsoft.com/setup/visual-studio-setup-powershell-module-available/
 
-$instance = Get-VSSetupInstance -All -Prerelease | Select-VSSetupInstance -Require 'Microsoft.Component.MSBuild' -Latest
-$installDir = $instance.installationPath
-Write-Host "Visual Studio is found at $installDir"
+# $instance = Get-VSSetupInstance -All -Prerelease | Select-VSSetupInstance -Require 'Microsoft.Component.MSBuild' -Latest
+# $installDir = $instance.installationPath
+# Write-Host "Visual Studio is found at $installDir"
 
-$msBuild = $installDir + '\MSBuild\Current\Bin\MSBuild.exe' # VS2019
-if (![System.IO.File]::Exists($msBuild)) {
-    $msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe' # VS2017
-    if (![System.IO.File]::Exists($msBuild)) {
-        Write-Host "MSBuild doesn't exist. Exit."
-        exit 1
-    }
-}
+# $msBuild = $installDir + '\MSBuild\Current\Bin\MSBuild.exe' # VS2019
+# if (![System.IO.File]::Exists($msBuild)) {
+#     $msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe' # VS2017
+#     if (![System.IO.File]::Exists($msBuild)) {
+#         Write-Host "MSBuild doesn't exist. Exit."
+#         exit 1
+#     }
+# }
 
-Write-Host "MSBuild found. Compile the projects."
+# Write-Host "MSBuild found. Compile the projects."
 
-$params = '..\Iconic.sln', '-property:Configuration=Release', '-verbosity:minimal', '-p:PostBuildEventUseInBuild=false'
+# $params = '..\Iconic.sln', '-property:Configuration=Release', '-verbosity:minimal', '-p:PostBuildEventUseInBuild=false'
 
-& $msBuild $params
+# & $msBuild $params
 
 # Create Nuget package
-.\nuget.exe pack -Build ..\Iconic\Iconic.csproj
+.\nuget.exe pack ..\Iconic\Iconic.nuspec -Build -Properties Configuration=Release -Verbosity detailed
 Write-Host "Nuget package created."
 
 # Create Umbraco package
