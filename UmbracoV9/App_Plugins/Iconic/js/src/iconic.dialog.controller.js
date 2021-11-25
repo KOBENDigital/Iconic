6332955a9bbfb05924480505969c888fa51f5e8d
Mario Lopez <mario@luminary.com>
2021-11-26 09:28:30 +1100

    fixes for nuget

---
+++ UmbracoV9/App_Plugins/Iconic/js/src/iconic.dialog.controller.js
@@ -0,0 +1,56 @@
+﻿angular.module("umbraco")
+    .controller("Koben.Iconic.Dialog.Controller",
+        ['$scope', 'assetsService', 'umbRequestHelper', function ($scope, assetsService, umbRequestHelper) {
+
+        $scope.packages = $scope.model.pickerConfig.packages;
+        $scope.pckgselected = null;
+
+
+        $scope.iconsSize = 16;
+        $scope.styles = [];
+        $scope.loading = false;
+
+        $scope.loadPackage = function (pckg) {
+            
+			if(pckg == null) return;
+			
+			$scope.loading = true;
+
+		
+            assetsService.loadCss('~/' + pckg.cssfile).then(function () {
+                $scope.loading = false;
+                $scope.pckgselected = pckg;
+            });
+
+        }
+
+
+
+        $scope.selectIcon = function (icon) {
+            $scope.model.pickerData = new Icon(icon, $scope.pckgselected.id);
+            $scope.submitForm($scope.model); //it passes the model back to the overlay caller
+            $scope.closeOverLay();
+        }
+
+        
+
+        function initOverlay() {
+            var pckg;
+
+            if ($scope.model.pickerData && $scope.model.pickerData.packageId) {
+                pckg = $scope.model.pickerConfig.packages.find((el) => el.id == $scope.model.pickerData.packageId);
+            }
+
+            //if there is only one package we select that one, regardless what the stored values says.
+            if ($scope.packages.length === 1) {
+                pckg = $scope.packages[0]; 
+            }
+
+            if (angular.isObject(pckg)) {
+                $scope.loadPackage(pckg);
+            }
+        }
+
+        initOverlay();
+
+    }]);
---
+++ UmbracoV9/App_Plugins/Iconic/js/src/iconic.prevalues.controller.js
@@ -0,0 +1,47 @@
+﻿angular.module("umbraco").controller("Koben.Iconic.Prevalues.Packages", [
+    "$scope",
+    "$http",
+    "editorService",
+    "umbRequestHelper",
+    function ($scope, $http, editorService, umbRequestHelper) {
+        $scope.overrideBgTemplate = false;
+
+        if (!angular.isArray($scope.model.value)) $scope.model.value = [];
+
+
+        $scope.createNewPackage = function () {
+            var newItem = new Package();
+
+            editorService.open({
+                title: "Create new package",
+                view: umbRequestHelper.convertVirtualToAbsolutePath("~/app_plugins/iconic/views/iconic.edit.dialog.html"),
+                saved: function () {
+                    $scope.model.value.push(angular.copy(newItem));
+                },
+                package: newItem
+            })
+
+        }
+
+        $scope.editPackage = function (pkg) {
+            editorService.open({
+                title: "Edit package",
+                view: umbRequestHelper.convertVirtualToAbsolutePath("~/app_plugins/iconic/views/iconic.edit.dialog.html"),
+                package: pkg
+            })
+        }
+
+        $scope.removeItem = function (index) {
+            $scope.model.value.splice(index, 1);
+        };
+
+        $scope.toggleItemDisplay = function (item) {
+            if ($scope.selectedItem === item) {
+                $scope.selectedItem = null;
+            } else {
+                $scope.selectedItem = item;
+            }
+        }
+
+    }
+]);
